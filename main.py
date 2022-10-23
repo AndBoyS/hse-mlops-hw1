00d61ff5014767bb1db561160c6d5bdbcf883df2
AndBoyS <andboyxd@gmail.com>
2022-10-23 17:09:06 +0300

    working predict

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from typing import *
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import make_pipeline
+from sklearn.exceptions import NotFittedError
 from flask import Flask
 from flask_restx import Api, Resource
 from werkzeug.datastructures import FileStorage
@@ -17,6 +19,7 @@ upload_parser.add_argument('file', location='files',
 
 feature_columns = ['age', 'embarked', 'pclass', 'sex']
 target_column = 'survived'
+all_columns = feature_columns + [target_column]
 
 model = make_pipeline(
     ColumnTransformer([
@@ -30,24 +33,40 @@ model = make_pipeline(
 
 @api.route('/train', methods=['PUT'])
 @api.expect(upload_parser)
-class MlMethods(Resource):
-    @api.doc(params={'file': 'Excel file'})
+class Train(Resource):
+    @api.doc(params={'file': f'Excel file with columns: {*all_columns,}'})
     def put(self):
         args = upload_parser.parse_args()
         data = pd.read_excel(args['file'])
-        X, y = self.prepare_data(data, train=True)
+        X, y = self.prepare_data(data)
         model.fit(X, y)
         return 'Training successful'
 
     @staticmethod
-    def prepare_data(df, train=True):
-        if train:
-            df = df[feature_columns + [target_column]].dropna()
-        else:
-            assert not df.isna().sum(), 'Nan data found'
+    def prepare_data(df: pd.DataFrame,
+                     ) -> Tuple[pd.DataFrame, pd.Series]:
+        df = df[all_columns].dropna()
+        return df[feature_columns], df[target_column]
 
-        X, y = df[feature_columns], df[target_column]
-        return X, y
+
+@api.route('/predict', methods=['POST'])
+@api.expect(upload_parser)
+class Predict(Resource):
+    @api.doc(params={'file': f'Excel file with columns: {*feature_columns,}'})
+    def post(self):
+        args = upload_parser.parse_args()
+        data = pd.read_excel(args['file'])
+        X = data[feature_columns]
+
+        if X.isna().sum().sum():
+            return 400, 'Nans found in data'
+
+        try:
+            pred = model.predict(X)
+        except NotFittedError:
+            return 400, "Model hasn't been fitted"
+
+        return {'prediction': pred.tolist()}
 
 
 if __name__ == '__main__':
